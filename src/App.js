6ba4f940ef22a7f108a8c56cb56ad2e78a70455d
ISCEvents <contact@iscevents.net>
2020-06-25 15:36:58 +0800

    added icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState, Fragment} from 'react';
 import './App.css';
 
+const URL = 'https://127.0.0.1:2999';
+const ENDPOINT = "/liveclientdata/allgamedata"; 
 const App  = () => {
 const [state, setState] = useState({data: []});
 
@@ -10,23 +12,61 @@ useEffect(()=>{
 
 const fetchData =  async () => {
     try {
+      /*
         const response = await fetch('https://127.0.0.1:2999/liveclientdata/allgamedata');
         if (!response.ok) {throw Error(response.statusText);}
         const json = await response.json();
         setState({ data: json, timer: JSON.stringify(json.gameData.gameTime), 
           allPlayers: JSON.stringify(json.allPlayers)});
         console.log(json);
+        */
+       const response = await fetch(URL + ENDPOINT);
+        if (!response.ok) {throw Error(response.statusText);}
+        const json = await response.json();
+        setState({ data: json, timer: JSON.stringify(json.gameData.gameTime), 
+          sumName: (json.allPlayers[0].summonerName),
+          champName: json.allPlayers[0].championName,
+          sumScores: Object.entries(json.allPlayers[0].scores)
+        });
+        //console.log(json);
     }
     catch (error) {console.log(error);}
 }
 
-console.log(state.allPlayers);
+//console.log(state.allPlayers);
 
+
+if (!state.sumScores) {
+  return (
+    <p> loading... </p>
+  ) 
+}
+
+let imgURL = "/champion/icon_" + state.champName + ".png";
+console.log(state.sumScores);
+let assists = state.sumScores[0][1];
+let creepScore = state.sumScores[1][1];
+let death = state.sumScores[2][1];
+let kills = state.sumScores[3][1];
 return (
   <Fragment>
-    <ul>
-      <li> {state.timer} </li>
-      {state.allPlayers}
+ {state.timer}
+    <ul>{state.sumName}
+      <li>{state.champName}</li>
+      <div> <img src={imgURL} /> </div>
+      <li>
+          Kills = {kills}
+      </li>
+      <li>
+          Assists = {assists}
+      </li>
+      <li>
+          Creepscore = {creepScore}
+      </li>
+      <li>
+          Deaths = {death}
+      </li>
+      
     </ul>
   </Fragment>
 );
